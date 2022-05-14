3c850952838a29c7c9bfb5a9e51956386d359332
Lonxyboi <drewthemachine22@gmail.com>
2022-05-14 11:08:05 -0400

    fixing html

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,4 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+var cityName = "Grand Rapids";
+var cities = [];
