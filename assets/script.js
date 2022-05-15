69fc2a836bad8b8e35b7bee9fd876742523eca60
Lonxyboi <drewthemachine22@gmail.com>
2022-05-15 11:31:52 -0400

    started js

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,4 @@
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 var cityName = "Grand Rapids";
 var cities = [];
+var cityFormEl = document.querySelector("#search-bar");
